def7fbb6ac3d05a46f970693eb22361ed7c78562
MamoonRashidRazmal <mamoonrashidrazmal@mamoonrzmalsair.localdomain>
2023-10-30 09:18:24 +0100

    the orders and much more

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../components/layout/Layout";
 import { useCart } from "../context/Cart";
 import { useAuth } from "../context/Buth";
 import { useNavigate } from "react-router-dom";
+import DropIn from "braintree-web-drop-in-react";
+
+import axios from "axios";
+import toast from "react-hot-toast";
 const CartPage = () => {
+  const [clientToken, setClientToken] = useState("");
+  const [instant, setInstant] = useState("");
   const [auth, setAuth] = useAuth();
+  const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
   const [cart, setCart] = useCart();
@@ -36,6 +43,45 @@ const CartPage = () => {
     }
   };
   console.log("auth data", auth);
+
+  ///payment
+  const getToken = async () => {
+    try {
+      const { data } = await axios.get(
+        "http://localhost:3000/api/v1/product/braintree/token"
+      );
+      //console.log("this data inside token sss", data.clientToken);
+      setClientToken(data?.clientToken);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getToken();
+  }, [auth?.token]);
+  ///handle payment
+
+  const handlePayment = async () => {
+    try {
+      setLoading(true);
+      const { nonce } = await instant.requestPaymentMethod();
+      const { data } = await axios.post(
+        "http://localhost:3000/api/v1/product/braintree/payment",
+        {
+          nonce,
+          cart,
+        }
+      );
+      setLoading(false);
+      localStorage.removeItem("cart");
+      setCart([]);
+      navigate("/dashboard/user/orders");
+      toast.success("Payment completed successfully");
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Layout>
       <div className="container">
@@ -82,8 +128,66 @@ const CartPage = () => {
           <div className="col-md-4 text-center">
             <h2>Cart Summary</h2>
             <p>Total |CheckOut |payment</p>
-            <h4>Total :{totalPrice()}</h4>
+
             <hr />
+            <h4>Total :{totalPrice()}</h4>
+            {auth?.user?.address ? (
+              <>
+                <div className="mb-3">
+                  <h4>Current Address</h4>
+                  <h5>{auth?.user?.address}</h5>
+                  <button
+                    className="btn btn-outline-warning"
+                    onClick={() => navigate("/dashboard/user/profile")}
+                  >
+                    Update Address
+                  </button>
+                </div>
+              </>
+            ) : (
+              <div className="mb-3">
+                {auth?.token ? (
+                  <button
+                    className="btn btn-outline-warning"
+                    onClick={() => navigate("/dashboard/user/profile")}
+                  >
+                    Update Address
+                  </button>
+                ) : (
+                  <button
+                    className="btn btn-outline-warning"
+                    onClick={() => navigate("/login", { state: "/cart" })}
+                  >
+                    Please log in to checkout
+                  </button>
+                )}
+              </div>
+            )}
+            <div className="mt-2">
+              {!clientToken || !auth?.token || !cart?.length ? (
+                ""
+              ) : (
+                <>
+                  <DropIn
+                    options={{
+                      authorization: clientToken,
+                      paypal: {
+                        flow: "vault",
+                      },
+                    }}
+                    onInstance={(instance) => setInstant(instance)}
+                  />
+
+                  <button
+                    className="btn btn-primary"
+                    onClick={handlePayment}
+                    disabled={loading || !instant || !auth?.user?.address}
+                  >
+                    {loading ? "Processing ...." : "Make Payment"}
+                  </button>
+                </>
+              )}
+            </div>
           </div>
         </div>
       </div>
--- src/pages/user/Orders.jsx
+++ src/pages/user/Orders.jsx
@@ -1,25 +1,66 @@
-import React from 'react'
-import Layout from '../../components/layout/Layout'
-import UserMenu from '../../components/layout/UserMenu';
-
+import React, { useState, useEffect } from "react";
+import Layout from "../../components/layout/Layout";
+import UserMenu from "../../components/layout/UserMenu";
+import axios from "axios";
+import { useAuth } from "../../context/Buth";
 const Orders = () => {
+  const [orders, setOrders] = useState([]);
+  const [auth, setAuth] = useAuth();
+  const getOders = async () => {
+    try {
+      const { data } = await axios.get(
+        "http://localhost:3000/api/v1/auth/orders"
+      );
+      setOrders(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    if (auth?.token) getOders();
+  }, [auth?.token]);
   return (
     <Layout title={"your Orders"}>
-      <div className='container-fluid p-3 m-3'>
-<div className='row'>
-    <div className="col-md-3">
-   < UserMenu/>
-    </div>
-    <div className="col-md-9">
-        <h1>All oders</h1>
-
-    </div>
-
-</div>
-
+      <div className="container-fluid p-3 m-3">
+        <div className="row">
+          <div className="col-md-3">
+            <UserMenu />
+          </div>
+          <div className="col-md-9">
+            <h1>All oders</h1>
+            {orders.map((o, i) => {
+              return (
+                <div className="border shadow" key={o._id}>
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <td scope="col">#</td>
+                        <td scope="col">status</td>
+                        <td scope="col">Buyer</td>
+                        <td scope="col">Date</td>
+                        <td scope="col">Payment</td>
+                        <td scope="col">Quantity</td>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th>{i + 1}</th>
+                        <th>{o?.status}</th>
+                        <th>{o?.buyer?.name}</th>
+                        <th>{o?.orders}</th>
+                        <th>{o?.payment.success ? "Sucess" : "Failed"}</th>
+                        <th>{o?.products?.length}</th>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Orders
+export default Orders;
