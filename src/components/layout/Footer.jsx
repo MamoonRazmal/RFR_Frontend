b8b11bff267ae2325f5a950a8cbde6712b9cdcf4
MamoonRashidRazmal <mamoonrashidrazmal@mamoonrzmalsair.localdomain>
2023-10-30 12:21:09 +0100

    first deplyment

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,15 +1,13 @@
-import {Link} from 'react-router-dom'
-const Footer=()=>{
-    return(
-        <div className="footer">
-            <h4 className="text-center">All right reserved &copy; Mamoon</h4>
-            <p className="text-center mt-3">
-
-                <Link to="/about" >About</Link>|
-                <Link to="/contact">Contact</Link>|
-                <Link to="/policy">Policy</Link>
-            </p>
-        </div>
-    )
-}
-export default Footer 
+import { Link } from "react-router-dom";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h4 className="text-center">All right reserved &copy; Mamoon and Nita</h4>
+      <p className="text-center mt-3">
+        <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
+        <Link to="/policy">Policy</Link>
+      </p>
+    </div>
+  );
+};
+export default Footer;
