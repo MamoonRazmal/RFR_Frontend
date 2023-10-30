16581d3d5bb1bc752a67fbf22ddff1ee4873687d
MamoonRashidRazmal <mamoonrashidrazmal@mamoonrzmalsair.localdomain>
2023-10-30 12:34:08 +0100

    first deplyment

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="footer">
-      <h4 className="text-center">All right reserved &copy; Mamoon and Nita</h4>
+      <h4 className="text-center">All right reserved &copy; N&M</h4>
       <p className="text-center mt-3">
         <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
         <Link to="/policy">Policy</Link>
