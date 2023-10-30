346ca40f9e511e26440e30c52b048e781f4ac171
MamoonRashidRazmal <mamoonrashidrazmal@mamoonrzmalsair.localdomain>
2023-10-30 18:08:26 +0100

    loading adding

--- src/pages/admin/AdminOrders.jsx
+++ src/pages/admin/AdminOrders.jsx
@@ -5,10 +5,13 @@ import toast from "react-hot-toast";
 import { useAuth } from "../../context/Buth";
 import axios from "axios";
 import { Select } from "antd";
+import { Hourglass } from "react-loader-spinner";
 const { Option } = Select;
+import moment from "moment";
 const AdminOrders = () => {
   const [orders, setOrders] = useState([]);
   const [auth, setAuth] = useAuth("");
+  const [loading, setLoading] = useState(false);
   const [status, setStatus] = useState([
     "Not Process",
     "Processing",
@@ -19,11 +22,14 @@ const AdminOrders = () => {
   const [changeStatus, setChangeStatus] = useState("");
   const getOrders = async () => {
     try {
+      setLoading(true);
       const { data } = await axios.get(
         "https://rfr-backend.onrender.com/api/v1/auth/all-orders"
       );
       setOrders(data);
+      setLoading(false);
     } catch (error) {
+      setLoading(false);
       console.log(error);
     }
   };
@@ -32,12 +38,14 @@ const AdminOrders = () => {
   }, [auth?.token]);
   const handleChange = async (orderId, value) => {
     try {
+      setLoading(true);
       const { data } = await axios.put(
         `https://rfr-backend.onrender.com/api/v1/auth/order-status/${orderId}`,
         {
           status: value,
         }
       );
+      setLoading(false);
       getOrders();
     } catch (error) {
       console.log(error);
@@ -45,76 +53,91 @@ const AdminOrders = () => {
   };
   return (
     <>
-      <Layout title="All order Data" />
-      <div className="row">
-        <div className="col-md-3">
-          <AdminMenu />
+      {loading ? (
+        <div className="loader">
+          <Hourglass
+            visible={true}
+            position="center"
+            height="80"
+            width="80"
+            ariaLabel="hourglass-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            colors={["#306cce", "#72a1ed"]}
+          />
         </div>
-        <div className="col-md-9">
-          <h1 className="text-center">All orders</h1>
-          {orders.map((o, i) => {
-            return (
-              <div className="border shadow" key={o._id}>
-                <table className="table">
-                  <thead>
-                    <tr>
-                      <th scope="col">#</th>
-                      <th scope="col">status</th>
-                      <th scope="col">Buyer</th>
-                      <th scope="col">Date</th>
-                      <th scope="col">Payment</th>
-                      <th scope="col">Quantity</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>{i + 1}</td>
-                      <td>
-                        <Select
-                          bordered={false}
-                          onChange={(value) => handleChange(o?._id, value)}
-                          defaultValue={o?.status}
-                        >
-                          {status.map((s, i) => (
-                            <Option key={i} value={s}>
-                              {s}
-                            </Option>
-                          ))}
-                        </Select>
-                      </td>
-                      <td>{o?.buyer?.name}</td>
-                      <td>{o?.CreatedAt}</td>
-                      <td>{o?.payment.success ? "Success" : "Failed"}</td>
-                      <td>{o?.products?.length}</td>
-                    </tr>
-                  </tbody>
-                </table>
-
-                <div className="container">
-                  {o?.products.map((f, i) => (
-                    <div className="row mb-2 p-3 card flex-row" key={f._id}>
-                      <div className="col-md-4">
-                        <img
-                          src={`https://rfr-backend.onrender.com/api/v1/product/product-photo/${f._id}`}
-                          className="card-img-top"
-                          alt={f.name}
-                          width="100px"
-                          height={"100px"}
-                        />
-                      </div>
-                      <div className="col-md-8">
-                        <p>{f.name}</p>
-                        <p>{f.description.substring(0, 30)}</p>
-                        <p>Price {f.price}</p>
-                      </div>
+      ) : (
+        <Layout title={"All Orders Data"}>
+          <div className="row dashboard">
+            <div className="col-md-3">
+              <AdminMenu />
+            </div>
+            <div className="col-md-9">
+              <h1 className="text-center">All Orders</h1>
+              {orders?.map((o, i) => {
+                return (
+                  <div className="border shadow" key={o._id}>
+                    <table className="table">
+                      <thead>
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">Status</th>
+                          <th scope="col">Buyer</th>
+                          <th scope="col"> date</th>
+                          <th scope="col">Payment</th>
+                          <th scope="col">Quantity</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>{i + 1}</td>
+                          <td>
+                            <Select
+                              bordered={false}
+                              onChange={(value) => handleChange(o._id, value)}
+                              defaultValue={o?.status}
+                            >
+                              {status.map((s, i) => (
+                                <Option key={i} value={s}>
+                                  {s}
+                                </Option>
+                              ))}
+                            </Select>
+                          </td>
+                          <td>{o?.buyer?.name}</td>
+                          <td>{moment(o?.createAt).fromNow()}</td>
+                          <td>{o?.payment.success ? "Success" : "Failed"}</td>
+                          <td>{o?.products?.length}</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <div className="container">
+                      {o?.products?.map((p, i) => (
+                        <div className="row mb-2 p-3 card flex-row" key={p._id}>
+                          <div className="col-md-4">
+                            <img
+                              src={`https://rfr-backend.onrender.com/api/v1/product/product-photo/${p._id}`}
+                              className="card-img-top"
+                              alt={p.name}
+                              width="100px"
+                              height={"100px"}
+                            />
+                          </div>
+                          <div className="col-md-8">
+                            <p>{p.name}</p>
+                            <p>{p.description.substring(0, 30)}</p>
+                            <p>Price : {p.price}</p>
+                          </div>
+                        </div>
+                      ))}
                     </div>
-                  ))}
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </Layout>
+      )}
     </>
   );
 };
--- src/pages/admin/Products.jsx
+++ src/pages/admin/Products.jsx
@@ -4,21 +4,26 @@ import Layout from "../../components/layout/Layout";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
+import { Hourglass } from "react-loader-spinner";
 const Products = () => {
+  const [loading, setLoading] = useState(false);
   const [product, setProduct] = useState([]);
 
   const getAllProducts = async () => {
     try {
+      setLoading(true);
       const { data } = await axios.get(
         "https://rfr-backend.onrender.com/api/v1/product/get-product"
       );
       console.log("this is inside product", data.success);
       if (data.success) {
+        setLoading(false);
         setProduct(data.products);
         toast.success("got all the product");
         console.log("this is inside product in if statment", data.products);
       }
     } catch (error) {
+      setLoading(false);
       console.log(error);
       toast.error("something went wrong");
     }
@@ -29,40 +34,57 @@ const Products = () => {
     console.log("this use state and useeffec4t ", product);
   }, []);
   return (
-    <Layout>
-      <div className="row">
-        <div className="col-md-3">
-          <AdminMenu />
+    <>
+      {loading ? (
+        <div className="loader">
+          <Hourglass
+            visible={true}
+            position="center"
+            height="80"
+            width="80"
+            ariaLabel="hourglass-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            colors={["#306cce", "#72a1ed"]}
+          />
         </div>
-        <div className="col-md-9 ">
-          <h1 className="text-center">All Products List</h1>
+      ) : (
+        <Layout>
+          <div className="row">
+            <div className="col-md-3">
+              <AdminMenu />
+            </div>
+            <div className="col-md-9 ">
+              <h1 className="text-center">All Products List</h1>
 
-          <div className="d-flex flex-wrap">
-            {product?.map((p) => (
-              <>
-                <Link
-                  to={`/dashboard/admin/product/${p.slug}`}
-                  key={p._id}
-                  className="product-link"
-                >
-                  <div className="card m-2" style={{ width: "18rem" }}>
-                    <img
-                      src={`https://rfr-backend.onrender.com/api/v1/product/product-photo/${p._id}`}
-                      className="card-img-top"
-                      alt={p.name}
-                    />
-                    <div className="card-body">
-                      <h5 className="card-title">{p.name}</h5>
-                      <p className="card-text">{p.description}</p>
-                    </div>
-                  </div>
-                </Link>
-              </>
-            ))}
+              <div className="d-flex flex-wrap">
+                {product?.map((p) => (
+                  <>
+                    <Link
+                      to={`/dashboard/admin/product/${p.slug}`}
+                      key={p._id}
+                      className="product-link"
+                    >
+                      <div className="card m-2" style={{ width: "18rem" }}>
+                        <img
+                          src={`https://rfr-backend.onrender.com/api/v1/product/product-photo/${p._id}`}
+                          className="card-img-top"
+                          alt={p.name}
+                        />
+                        <div className="card-body">
+                          <h5 className="card-title">{p.name}</h5>
+                          <p className="card-text">{p.description}</p>
+                        </div>
+                      </div>
+                    </Link>
+                  </>
+                ))}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </Layout>
+        </Layout>
+      )}
+    </>
   );
 };
 
